3d563c68f826c3832b77a0645bb927916443ee68
Dana Goosen <klymentovska@gmail.com>
2025-05-07 09:24:47 +0200

    Create a monster & the opponent

--- code/main.py
+++ code/main.py
@@ -1,6 +1,8 @@
 from settings import *
 from support import *
 from timer import Timer
+from monster import Monster, Opponent
+from random import choice
 
 class Game:
     def __init__(self):
@@ -9,9 +11,23 @@ class Game:
         pygame.display.set_caption('Monster Battle')
         self.clock = pygame.time.Clock()
         self.running = True
+        self.import_assets()
 
         # groups 
         self.all_sprites = pygame.sprite.Group()
+        
+        # data
+        player_monster_list = ['Sparchu', 'Cleaf', 'Jacana']
+        self.player_monsters = [Monster(name, self.back_surfs[name]) for name in player_monster_list]
+        self.monster = self.player_monsters[0]
+        self.all_sprites.add(self.monster)
+        opponent_name = choice(list(MONSTER_DATA.keys()))
+        self.opponent = Opponent(opponent_name, self.front_surfs[opponent_name], self.all_sprites)
+        
+    def import_assets(self):
+        self.front_surfs = folder_importer('images', 'front')
+        self.back_surfs = folder_importer('images', 'back')
+        self.bg_surfs = folder_importer('images', 'other')
 
     def run(self):
         while self.running:
@@ -24,6 +40,7 @@ class Game:
             self.all_sprites.update(dt)
 
             # draw  
+            self.display_surface.blit(self.bg_surfs['bg'], (0,0))
             self.all_sprites.draw(self.display_surface)
             pygame.display.update()
         
