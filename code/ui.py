ad09b5649502c7eb704150abc7493e128bedae89
Dana Goosen <klymentovska@gmail.com>
2025-05-09 08:59:24 +0200

    Make the main menu more flexible

--- code/ui.py
+++ code/ui.py
@@ -11,30 +11,38 @@ class UI:
         # control
         self.general_options = ['attack', 'heal', 'switch', 'escape']
         self.general_index = {'col': 0, 'row': 0}
+        self.attack_index = {'col': 0, 'row': 0}
         self.state = 'general'
+        self.rows, self.cols = 2,2
         
     def input(self):
         keys = pygame.key.get_just_pressed()
-        self.general_index['row'] = (self.general_index['row'] + int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])) % 2
-        self.general_index['col'] = (self.general_index['col'] + int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])) % 2
-        if keys[pygame.K_SPACE]:
-            self.state = self.general_options[self.general_index['col'] + self.general_index['row'] * 2]
+        if self.state == 'general':
+            self.general_index['row'] = (self.general_index['row'] + int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])) % self.rows
+            self.general_index['col'] = (self.general_index['col'] + int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])) % self.cols
+            if keys[pygame.K_SPACE]:
+                self.state = self.general_options[self.general_index['col'] + self.general_index['row'] * 2]
+                
+        if self.state == 'attack':
+            self.attack_index['row'] = (self.attack_index['row'] + int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])) % self.rows
+            self.attack_index['col'] = (self.attack_index['col'] + int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])) % self.cols
+            if keys[pygame.K_SPACE]:
+                print(self.monster.abilities[self.attack_index['col'] + self.attack_index['row'] * 2])
             
-    def general(self):
+    def quad_select(self, index, options):
         # bg
         rect = pygame.FRect(self.left + 40, self.top + 60, 400,200)
         pygame.draw.rect(self.display_surface, COLORS['white'], rect, 0, 4)
         pygame.draw.rect(self.display_surface, COLORS['gray'], rect, 4, 4)
     
         # menu
-        cols, rows = 2, 2
-        for col in range(cols):
-            for row in range(rows):
+        for col in range(self.cols):
+            for row in range(self.rows):
                 i = col + 2 * row
-                color = COLORS['gray'] if col == self.general_index['col'] and row == self.general_index['row'] else COLORS['black']
-                x = rect.left + rect.width / 4 + (rect.width / 2) * col
-                y = rect.top + rect.height / 4 + (rect.height / 2) * row
-                text_surf = self.font.render(self.general_options[i], True, color)
+                color = COLORS['gray'] if col == index['col'] and row == index['row'] else COLORS['black']
+                x = rect.left + rect.width / (self.cols * 2) + (rect.width / self.cols) * col
+                y = rect.top + rect.height / (self.rows * 2) + (rect.height / self.rows) * row
+                text_surf = self.font.render(options[i], True, color)
                 text_rect = text_surf.get_frect(center = (x,y))
                 self.display_surface.blit(text_surf, text_rect)
     
@@ -43,5 +51,5 @@ class UI:
                  
     def draw(self):
         match self.state:
-            case 'general':
-                self.general()
+            case 'general': self.quad_select(self.general_index, self.general_options)
+            case 'attack': self.quad_select(self.attack_index, self.monster.abilities)
